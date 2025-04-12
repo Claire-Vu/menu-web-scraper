cd7cbb90aa6184cbccf50fccd52a8e7d67c7dd00
Claire Vu <v.claire47@gmail.com>
2025-04-12 10:31:32 -0700

    feat: added basic get request for a web page using axios

--- web-scraper.js
+++ web-scraper.js
@@ -2,9 +2,14 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 axios
-    .get('https://www.texasroadhouse.com/location/204-maranaaz/menu')
+    // Issue 1: TexasRoadhouse error: does not allow web scaping
+    // Issue 2: Sushi Garden menu is pdf format
+    .get('https://sushigarden.com/menu/new-menus/broadway.pdf')
     .then ((response) => {
-        console.log(response.data);
+        // console.log(response.data)
+        const $ = cheerio.load(response.data);
+        const title = $('h1').text();
+        console.log(`Page Title: ${title}`);
 
     })
     .catch((error) => {
